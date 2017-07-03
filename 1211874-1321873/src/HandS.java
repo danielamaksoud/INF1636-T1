1dd229ad97b9903aee532163fa3b379868cc8b77
fvbizzo <felipe.bizzo@gmail.com>
2017-07-02 22:26:03 -0300

    Add files via upload

--- 1211874-1321873/src/HandS.java
+++ 1211874-1321873/src/HandS.java
@@ -1,24 +1,29 @@
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.net.URL;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class HandS extends JFrame{
 
 	backgroundimage tela;
 	
-	public HandS(String nome, ArrayList<String> mao) {
+	public HandS(String nome, ArrayList<String> mao, int numcar) {
 		super(nome);
-		int i = 0;
-		String buffer;
-		setLayout(new GridLayout(1,1));
-		this.setResizable(false);
-		while((buffer = mao.get(i)) != null)
-		{
-			//Image img = new Image();
-		}
+		JFrame j = new JFrame();
+		tela = new backgroundimage(mao.get(numcar)+".jpg");
+		getContentPane().add(tela);
+		j.pack();
+
 		
+	}
+
+	private Image getImage(URL url) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
